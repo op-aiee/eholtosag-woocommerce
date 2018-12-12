14a3e3f81b7a9f8341a406f8f6aa37af1695fdd8
calvez <nemeth.lorant@gmail.com>
2018-12-12 15:10:59 +0100

    initial

---
+++ includes/eholtosag-woocommerce-functions.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * WooCommerce Extension Functionality
+ *
+ * @category  Class
+ * @package   WordPress
+ * @author    Lorant Nemeth
+ * @license
+ * @link
+ */
+
+if ( ! defined( 'ABSPATH' ) ) { exit; // Exit if accessed directly.
+}
+
+/**
+ * Class to manage breadcrumbs and vendor's custom fields.
+ */
+class WooCommerce_Extension_Functionality {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		//
+	}
+
+}
+
+return new WooCommerce_Extension_Functionality();
